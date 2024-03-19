5fb0cb8a264aa54bc0fdb1b8f10da6cd513d381b
CoffeerLatte <todty2312@gmail.com>
2024-03-19 15:43:03 +0900

    test commit

--- src/test/java/main/spring/login/t4orderlogin/ApplicationTests.java
+++ src/test/java/main/spring/login/t4orderlogin/ApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package main.spring.login.t4orderlogin;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class ApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
