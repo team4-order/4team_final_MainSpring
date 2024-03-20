cc5bba4084c23ca54cdf84882f24690bae81c40b
CoffeerLatte <todty2312@gmail.com>
2024-03-20 09:35:32 +0900

    commit login page project

---
+++ main-Login/src/main/java/main/spring/login/mainlogin/MainLoginApplication.java
@@ -0,0 +1,13 @@
+package main.spring.login.mainlogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainLoginApplication.class, args);
+	}
+
+}
---
+++ main-Login/src/test/java/main/spring/login/mainlogin/MainLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package main.spring.login.mainlogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
