3b9d8ffedbed3f6e1b294f60ce51b4ca46609ce3
CoffeerLatte <todty2312@gmail.com>
2024-03-21 17:12:06 +0900

    commit changes

---
+++ main-Login/src/main/java/main/spring/login/security/SpringSecurityConfig.java
@@ -0,0 +1,15 @@
+package main.spring.login.security;
+
+import io.jsonwebtoken.JwtBuilder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SpringSecurityConfig {
+
+  //  private final
+
+}
