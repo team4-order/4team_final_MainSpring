95299e718b041ceb72bddbfc93628fecfb8203da
seulle <seull@LAPTOP-L9AOK14Q.>
2024-03-22 19:38:09 +0900

    창고별 목록 완료

--- demo2/src/main/java/main/spring/login/demo2/controller/InventoryController.java
+++ demo2/src/main/java/main/spring/login/demo2/controller/InventoryController.java
@@ -4,8 +4,8 @@ import main.spring.login.demo2.entity.Inventory;
 import main.spring.login.demo2.service.InventoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -26,8 +26,12 @@ public class InventoryController {
     }
 
     // 창고 코드를 파라미터로 받아 해당 창고에 속한 재고 목록을 반환하는 엔드포인트 추가
-    @GetMapping(params = "storageCode")
-    public List<Inventory> getInventoriesByStorageCode(@RequestParam String storageCode) {
+//    @GetMapping(params = "storageCode")
+    @GetMapping("/read/{storageCode}")
+//    public List<Inventory> getInventoriesByStorageCode(@RequestParam String storageCode) {
+    public List<Inventory> getInventoriesByStorageCode(@PathVariable String storageCode) {
         return service.findByStorageCode(storageCode);
     }
+
+
 }
