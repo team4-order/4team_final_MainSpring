0804c1256064751758138a2dd42b099da6cfd1f8
CoffeerLatte <todty2312@gmail.com>
2024-03-25 14:10:08 +0900

    commit base code

---
+++ main-login-2/src/main/java/main/spring/login/mainlogin2/MainLogin2Application.java
@@ -0,0 +1,13 @@
+package main.spring.login.mainlogin2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainLogin2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainLogin2Application.class, args);
+	}
+
+}
---
+++ main-login-2/src/test/java/main/spring/login/mainlogin2/MainLogin2ApplicationTests.java
@@ -0,0 +1,13 @@
+package main.spring.login.mainlogin2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainLogin2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
