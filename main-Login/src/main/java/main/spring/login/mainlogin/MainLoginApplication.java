99a31dd6f2a5dea2c25655797cb8f062853a2e97
CoffeerLatte <todty2312@gmail.com>
2024-03-25 14:10:43 +0900

    commit  base code p3

--- main-Login/src/main/java/main/spring/login/mainlogin/MainLoginApplication.java
+++ main-Login/src/main/java/main/spring/login/mainlogin/MainLoginApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
-public class MainLoginApplication {:ㅈㅂ
+public class MainLoginApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MainLoginApplication.class, args);
---
+++ main-login3/src/main/java/main/spring/login/mainlogin3/MainLogin3Application.java
@@ -0,0 +1,13 @@
+package main.spring.login.mainlogin3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainLogin3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainLogin3Application.class, args);
+    }
+
+}
