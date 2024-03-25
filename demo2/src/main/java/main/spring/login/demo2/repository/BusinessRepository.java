3aca5488569b7dd8fd4dc32b19af198878ed4436
ae-chae <aechae11@gmail.com>
2024-03-25 11:31:56 +0900

    [Fix]

--- demo2/src/main/java/main/spring/login/demo2/repository/BusinessRepository.java
+++
@@ -1,8 +0,0 @@
-package main.spring.login.demo2.repository;
-
-import main.spring.login.demo2.entity.Business;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BusinessRepository extends JpaRepository<Business, String> {
-
-}
