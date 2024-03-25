4e43effd68bc369f074fc24a8844228461f4d637
qkralsgml78 <qkralsgml78@gmail.com>
2024-03-25 09:19:41 +0900

    [Add] JPA 설정

---
+++ demo2/src/test/java/main/spring/login/demo2/Demo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package main.spring.login.demo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
