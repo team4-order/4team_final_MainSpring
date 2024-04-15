4037d63e8dc3b0d8140161f3150a391b7db10f6d
seulle <seull@LAPTOP-L9AOK14Q.>
2024-04-15 21:56:25 +0900

    Test

--- demo2/src/main/java/main/spring/login/demo2/Demo2Application.java
+++ demo2/src/main/java/main/spring/login/demo2/Demo2Application.java
@@ -2,13 +2,12 @@ package main.spring.login.demo2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 public class Demo2Application {
 
-
     public static void main(String[] args) {
         SpringApplication.run(Demo2Application.class, args);
     }
+
 }
