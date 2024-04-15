c2a1310ef2d061beedd71dfa4a879c64fcb23b55
seulle <seull@LAPTOP-L9AOK14Q.>
2024-04-15 22:11:36 +0900

    머지 충돌 수정

--- demo2/src/main/java/main/spring/login/demo2/controller/CStorageController.java
+++ demo2/src/main/java/main/spring/login/demo2/controller/CStorageController.java
@@ -3,7 +3,6 @@ package main.spring.login.demo2.controller;
 import main.spring.login.demo2.dto.CStorageDTO;
 import main.spring.login.demo2.entity.CStorage;
 import main.spring.login.demo2.entity.Contact;
-import main.spring.login.demo2.repository.ContactRepository;
 import main.spring.login.demo2.service.CStorageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
