1171e3f986cb076e7e14b385f430a84afa10897c
dmario24 <becky2sawyer@gmail.com>
2024-04-16 10:43:03 +0900

    Pilot application of checkstyle guide

--- demo2/src/main/java/main/spring/login/demo2/repository/OrderProductRepository.java
+++ demo2/src/main/java/main/spring/login/demo2/repository/OrderProductRepository.java
@@ -9,17 +9,15 @@ import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface OrderProductRepository extends JpaRepository<OrderProduct, Integer>{
-    List<OrderProduct> findByOrderNumber(Integer orderNumber);
+public interface OrderProductRepository extends JpaRepository<OrderProduct, Integer> {
+  List<OrderProduct> findByOrderNumber(Integer orderNumber);
 
-    @Query("SELECT new main.spring.login.demo2.dto.OrderProductDto(op.orderNumber, op.goodsCode, gm.goodsName, op.goodsGrade, op.orderQuantity, op.orderPrice) " +
-            "FROM OrderProduct op LEFT JOIN GoodsMaster gm ON op.goodsCode = gm.goodsCode WHERE op.orderNumber = :orderNumber")
-    List<OrderProductDto> findOrderProductByOrderNumber(@Param("orderNumber") Integer orderNumber);
+  @Query("SELECT new main.spring.login.demo2.dto.OrderProductDto(op.orderNumber, op.goodsCode, gm.goodsName, op.goodsGrade, op.orderQuantity, op.orderPrice) "
+        + "FROM OrderProduct op LEFT JOIN GoodsMaster gm ON op.goodsCode = gm.goodsCode WHERE op.orderNumber = :orderNumber")
+  List<OrderProductDto> findOrderProductByOrderNumber(@Param("orderNumber") Integer orderNumber);
 
-    //OrderProductYDto(Integer orderNumber, String goodsCode, String goodsGrade, Integer orderQuantity, String orderStatus, String storageCode)
-    @Query("SELECT new main.spring.login.demo2.dto.OrderProductYDto(op.orderNumber, op.goodsCode, op.goodsGrade, op.orderQuantity, om.orderStatus, om.storageCode)" +
-            "FROM OrderMaster om RIGHT OUTER JOIN OrderProduct op ON om.orderNumber = op.orderNumber WHERE om.storageCode = :storageCode AND (om.orderStatus = '출고 준비 중' OR om.orderStatus = '주문 완료')")
-    List<OrderProductYDto> findOrderProductByStorageCode(@Param("storageCode") String storageCode);
-
-
+  //OrderProductYDto(Integer orderNumber, String goodsCode, String goodsGrade, Integer orderQuantity, String orderStatus, String storageCode)
+  @Query("SELECT new main.spring.login.demo2.dto.OrderProductYDto(op.orderNumber, op.goodsCode, op.goodsGrade, op.orderQuantity, om.orderStatus, om.storageCode)"
+        + "FROM OrderMaster om RIGHT OUTER JOIN OrderProduct op ON om.orderNumber = op.orderNumber WHERE om.storageCode = :storageCode AND (om.orderStatus = '출고 준비 중' OR om.orderStatus = '주문 완료')")
+  List<OrderProductYDto> findOrderProductByStorageCode(@Param("storageCode") String storageCode);
 }
