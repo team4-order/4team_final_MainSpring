78dc4e786d0df49916ef5d1cfa4e9616763b535b
dmario24 <becky2sawyer@gmail.com>
2024-04-16 09:29:46 +0900

    Add Code Analysis Plug-in

---
+++ demo2/src/test/java/main/spring/login/demo2/BackendApplicationTest.java
@@ -0,0 +1,12 @@
+package main.spring.login.demo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTest {
+
+  @Test
+  void contextLoads() {
+  }
+}
